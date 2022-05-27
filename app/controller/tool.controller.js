b74bd6976fa888a03f47057742a36dab614fb8f4
El <richard.black183@gmail.com>
2022-05-27 17:43:45 +0300

    fix

--- app/controller/tool.controller.js
+++ app/controller/tool.controller.js
@@ -4,8 +4,9 @@ const path = require("path");
 exports.upload = async (req, res) => {
   var img = req?.body?.uri;
   var ext = img.split(";")[0].match(/jpeg|png|gif/)[0];
-  var buf = Buffer.from(img.split(","[0]), "base64");
-  let filePath = `app/uploads/${Date.now()}.${ext}`;
+
+  var buf = Buffer.from(img.split(",")[1], "base64");
+  let filePath = `app/uploads/${Date.now()}.${ext}`; //
   const fileName = path.join(global.appRoot, filePath);
   fs.writeFileSync(fileName, buf);
   res.send(`${fileName}`);
