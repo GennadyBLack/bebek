2b3f68c18bef86b7e0e7aba53b357d98aa712b50
El <richard.black183@gmail.com>
2022-06-03 15:37:43 +0300

    verify error

--- app/middleware/chekToken.js
+++ app/middleware/chekToken.js
@@ -2,14 +2,19 @@ const jwt = require("jsonwebtoken");
 const tokenSecret = "my-token-secret";
 
 exports.verify = (req, res, next) => {
-  const token = req.headers.authorization;
-  if (!token || token == "null" || token == null)
-    res.status(403).json({ error: "please provide a token" });
-  else {
-    jwt.verify(token.split(" ")[1], tokenSecret, (err, value) => {
-      if (err) res.status(500).json({ error: "failed to authenticate token" });
-      req.user = value?.data;
-      next();
-    });
+  try {
+    const token = req.headers.authorization;
+    if (!token || token == "null" || token == null)
+      res.status(403).json({ error: "please provide a token" });
+    else {
+      jwt.verify(token.split(" ")[1], tokenSecret, (err, value) => {
+        if (err)
+          res.status(500).json({ error: "failed to authenticate token" });
+        req.user = value?.data;
+        next();
+      });
+    }
+  } catch (error) {
+    res.status(400).send(error);
   }
 };
