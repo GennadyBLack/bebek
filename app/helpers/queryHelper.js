994193718471c6e2053959aed47066581e17677a
aikhsanov <adrstrings@gmail.com>
2022-06-08 09:56:37 +0300

    !fix queryHelper.js

--- app/helpers/queryHelper.js
+++ app/helpers/queryHelper.js
@@ -2,12 +2,20 @@ const searchBuilder = require("sequelize-search-builder");
 const db = require("../config/db.config");
 const queryHelper = async (model, req, res) => {
   // Set req.query param to Search Builder constructor
-
-  const search = new searchBuilder(db.Sequelize, req.query),
+  const query = {};
+  for (const key in req.query) {
+    if (typeof req.query[key] === "string") {
+      query[key] = JSON.parse(req.query[key]);
+    } else {
+      query[key] = req.query[key];
+    }
+  }
+  const search = new searchBuilder(db.Sequelize, query),
     whereQuery = search.getWhereQuery(),
     orderQuery = search.getOrderQuery(),
     limitQuery = search.getLimitQuery(),
     offsetQuery = search.getOffsetQuery();
+
   return res.send({
     data: await model.findAll({
       include: [{ all: true, nested: true }],
